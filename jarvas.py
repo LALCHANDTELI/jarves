73dd52dee8b3017ea5eaf1e895ac5d5d76b15c4c
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-11-23 21:14:56 +0530

    Update jarvas.py

--- jarvas.py
+++ jarvas.py
@@ -54,8 +54,8 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('top.clone.factory@gmail.com', '@cloneLAL13')
-    server.sendmail('top.clone.factory@gmail.com', to, content)
+    server.login('your_mail', 'your_password')
+    server.sendmail('your_mail', to, content)
     server.close()
 
 if __name__ == "__main__":
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'song path'
             songs = os.listdir(music_dir)
             print(songs)    
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -94,16 +94,16 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "vs code path"
             os.startfile(codePath)
 
         elif 'send a mail' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "lalchandteli13@gmail.com"    
+                to = "mail"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+                speak("Sorry, I am not able to send this email")    
